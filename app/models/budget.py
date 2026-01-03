21ce585a8cf48b3ecac04fe84c945ae1a3f468f1
surendarxyloite <surendar.xyloite.2025@gmail.com>
2026-01-03 10:36:43 +0530

    class budget has been created

--- app/models/budget.py
+++ app/models/budget.py
@@ -5,3 +5,9 @@ from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from app.db.database import Base
 
+class Budget(Base):
+    __tablename__ = "budgets"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    trip_id = Column(Integer, ForeignKey("trips.id"), nullable=False)
+    
