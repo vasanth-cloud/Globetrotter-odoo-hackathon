98a18237018bbad7156d7329d4cddb1a5b285d1d
Siddharth <siddharthofficialmain@gmail.com>
2026-01-03 12:09:27 +0530

    Inisialised Activity

--- app/models/activity.py
+++ app/models/activity.py
@@ -1,3 +1,11 @@
 from sqlalchemy import Column, Integer, String, Float, Text
 from sqlalchemy.orm import relationship
 from app.db.database import Base
+
+
+class Activity(Base):
+    _tablename_ = "activities"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, index=True)
+    category = Column(String, nullable=False) 
