4dcf2d77ce5089769d49035622659df870e767ed
surendarxyloite <surendar.xyloite.2025@gmail.com>
2026-01-03 10:48:07 +0530

    function defined for decoding access token

--- app/core/security.py
+++ app/core/security.py
@@ -24,3 +24,9 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        return payload
+    except JWTError:
+        return None
