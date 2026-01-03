12297270bf296177e6f7bf6f1957a23419653275
surendarxyloite <surendar.xyloite.2025@gmail.com>
2026-01-03 10:40:52 +0530

    security file is created and imported jwt

---
+++ app/core/security.py
@@ -0,0 +1,9 @@
+#app/core/security.py
+
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from app.core.config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
