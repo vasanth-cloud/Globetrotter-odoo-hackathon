7296805fea5deaad7433fbcdfc9acfa1e83f81a3
Siddharth <siddharthofficialmain@gmail.com>
2026-01-03 10:14:33 +0530

    Created User Entity

--- app/models/user.py
+++ app/models/user.py
@@ -2,3 +2,11 @@ from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.db.database import Base
+
+class User(Base):
+    _tablename_ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=False)
+   
