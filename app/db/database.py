d972c58546db1c4c9e84015090601e6246e78353
surendarxyloite <surendar.xyloite.2025@gmail.com>
2026-01-03 10:52:10 +0530

    function created for sessionlocal

--- app/db/database.py
+++ app/db/database.py
@@ -10,3 +10,9 @@ engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
